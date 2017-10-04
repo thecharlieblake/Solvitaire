8ed10c2b64e56b5e3df3f0c5117a0baec50d29f8
thecharlesblake <thecharlesblake@live.co.uk>
2017-10-04 11:53:16 +0100

    Add conan.io to project & add Boost to Hello World

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,15 @@
+#include <string>
 #include <iostream>
+#include <boost/foreach.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::string hello( "Hello, world!\n" );
+
+    BOOST_FOREACH( char ch, hello )
+    {
+        std::cout << ch;
+    }
+
     return 0;
 }
