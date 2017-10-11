a70e9a857ece06da65d7ce7c6d271dcd39968d55
thecharlesblake <thecharlesblake@live.co.uk>
2017-10-11 11:00:50 +0100

    Add basic logging to project

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,19 @@
 #include <string>
 #include <iostream>
 #include <boost/foreach.hpp>
+#include <boost/log/trivial.hpp>
+
+namespace logging = boost::log;
 
 int main()
 {
+
+#ifndef NDEBUG
+    BOOST_LOG_TRIVIAL(debug) << "Log debug";
+#endif
+    BOOST_LOG_TRIVIAL(info) << "Log info";
+    BOOST_LOG_TRIVIAL(warning) << "Log warning";
+
     std::string hello( "Hello, world!\n" );
 
     BOOST_FOREACH( char ch, hello )
