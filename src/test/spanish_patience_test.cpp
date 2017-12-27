6c4bdf37d6bebe3562df9c9b459e6b7036f21dea
thecharlesblake <thecharlesblake@live.co.uk>
2017-12-27 00:23:07 +0000

    All basic sp tests complete

--- src/test/spanish_patience_test.cpp
+++ src/test/spanish_patience_test.cpp
@@ -8,8 +8,21 @@
 
 typedef test_helper th;
 
+const static std::string path = "resources/spanish_patience/";
 const static std::string type = "simple-spanish-patience";
 
-TEST(SpanishPatience, SPTestName) {
-    EXPECT_TRUE(th::is_solvable("SPTestName", type));
+TEST(SpanishPatience, SimpleSolvable) {
+    EXPECT_TRUE(th::is_solvable(path + "SimpleSolvable.json", type));
+}
+
+TEST(SpanishPatience, ComplexSolvable) {
+    EXPECT_TRUE(th::is_solvable(path + "ComplexSolvable.json", type));
+}
+
+TEST(SpanishPatience, SimpleUnsolvable) {
+    EXPECT_FALSE(th::is_solvable(path + "SimpleUnsolvable.json", type));
+}
+
+TEST(SpanishPatience, ComplexUnsolvable) {
+    EXPECT_FALSE(th::is_solvable(path + "ComplexUnsolvable.json", type));
 }
--- src/test/test_helper.cpp
+++ src/test/test_helper.cpp
@@ -13,7 +13,7 @@ using namespace rapidjson;
 using namespace std;
 
 bool test_helper::is_solvable(const std::string& input_file, const std::string& preset_type) {
-    const Document in_doc = util::get_file_json("resources/" + input_file + ".json");
+    const Document in_doc = util::get_file_json(input_file);
     const sol_rules rules = sol_rules::from_preset(preset_type);
 
     game_state gs(rules, in_doc);
