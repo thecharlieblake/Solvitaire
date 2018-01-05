cc9a9721d3a7bdbbcc055986c014a0665d5b3e63
thecharlesblake <thecharlesblake@live.co.uk>
2018-01-05 18:01:17 +0000

    Add unit tests for somerset

---
+++ src/test/somerset_test.cpp
@@ -0,0 +1,28 @@
+//
+// Created by thecharlesblake on 10/4/17.
+//
+
+#include <gtest/gtest.h>
+
+#include "test_helper.h"
+
+typedef test_helper th;
+
+const static std::string path = "resources/somerset/";
+const static std::string type = "simple-somerset";
+
+TEST(Somerset, SimpleSolvable) {
+    EXPECT_TRUE(th::is_solvable(path + "SimpleSolvable.json", type));
+}
+
+TEST(Somerset, ComplexSolvable) {
+    EXPECT_TRUE(th::is_solvable(path + "ComplexSolvable.json", type));
+}
+
+TEST(Somerset, SimpleUnsolvable) {
+    EXPECT_FALSE(th::is_solvable(path + "SimpleUnsolvable.json", type));
+}
+
+TEST(Somerset, ComplexUnsolvable) {
+    EXPECT_FALSE(th::is_solvable(path + "ComplexUnsolvable.json", type));
+}
