2009f747848ae2d4af20a15fb3cb465e1ea112f1
thecharlesblake <thecharlesblake@live.co.uk>
2018-04-02 22:09:11 +0100

    Added logic for improved solvability calculations

--- src/main/solvability/solvability_calc.cpp
+++ src/main/solvability/solvability_calc.cpp
@@ -11,48 +11,119 @@
 
 using namespace std;
 
+///////////////////
+// SETUP METHODS //
+///////////////////
+
+const double solvability_calc::coeff_ewma_weight = 0.7;
 
 solvability_calc::solvability_calc(const sol_rules& r) : rules(r) {
-    reset();
 }
 
-void solvability_calc::reset() {
-    current_seed = 0;
-    timeout = chrono::milliseconds(1000);
-    solvable = 0;
-    unsolvable = 0;
-    intractable = 0;
-}
+//////////////////////
+// PRINTING METHODS //
+//////////////////////
 
-void solvability_calc::calculate_solvability_percentage() {
-    reset();
-    print_header();
-
-    while(current_seed++ < INT_MAX) {
-        solve_seed();
-
-        double lower_bound = sol_lower_bound();
-        double upper_bound = sol_upper_bound();
-
-        print_row(lower_bound, upper_bound);
-    }
-}
-
-void solvability_calc::print_header() const {
+void solvability_calc::print_header(long t) const {
     cout << "Calculating solvability percentage...\n\n"
-            "[Lower Bound, Upper Bound] | (Solvable/Unsolvable/Intractable) "
-            "| Current seed\n";
+            "[Lower Bound, Upper Bound] | Solvable/Unsolvable/Intractable "
+            "| Current seed | (Est. Confidence Interval: Continue/Re-eval)"
+            "\n--- Timeout = " << t << " milliseconds ---";
     cout << fixed << setprecision(3);
 }
 
-void solvability_calc::print_row(double lower_bound, double upper_bound) const {
-    cout << "[" << lower_bound * 100 << ", " << upper_bound * 100
-         << "] | ("<< solvable << "/" << unsolvable << "/" << intractable
-         << ") | " << current_seed << "\n";
+void solvability_calc::print_row(const seed_results& seed_res, int seed,
+                                 double cont, double re_eval) const {
+    double lower_bound = sol_lower_bound(seed_res.solvables(),
+                                         seed_res.unsolvables(),
+                                         seed_res.intractables());
+    double upper_bound = sol_upper_bound(seed_res.solvables(),
+                                         seed_res.unsolvables(),
+                                         seed_res.intractables());
+
+    if (cont != -1 && re_eval !=-1) {
+        cout << "\n"
+             << "["    << lower_bound * 100
+             << ", "   << upper_bound * 100
+             << "] | " << seed_res.solvables()
+             << "/"    << seed_res.unsolvables()
+             << "/"    << seed_res.intractables()
+             << " | "  << seed
+             << " | (";
+        if (seed_res.intractables() == 0) {
+            cout << "N/A";
+        } else {
+            cout  << cont * 100
+                  << ", " << re_eval * 100;
+        }
+        cout << ")";
+    } else {
+        cout << "\n"
+             << "[-- re-eval --] | " << seed_res.solvables()
+             << "/"    << seed_res.unsolvables()
+             << "/"    << seed_res.intractables()
+             << " | "  << seed;
+    }
 }
 
-void solvability_calc::solve_seed() {
-    game_state gs(rules, current_seed);
+void solvability_calc::print_begin_re_eval_msg(long t) const {
+    cout << "\n--- Timeout doubled to " << t
+         << " ms. Re-evaluating intractable seeds ---";
+}
+
+void solvability_calc::print_end_re_eval_msg() const {
+    cout << "\n--- Re-evaluation complete ---";
+}
+
+
+/////////////////////
+// SOLVING METHODS //
+/////////////////////
+
+void solvability_calc::calculate_solvability_percentage() {
+    int current_seed = 0;
+    millisec timeout(10);
+    inc_t_coeffs coeffs(0.4, 0.0, 1.0);
+
+    print_header(timeout.count());
+
+    seed_results seed_res;
+    while(current_seed < INT_MAX) {
+
+        double cont_ci_sz = continue_ci_size(coeffs, seed_res, timeout);
+        double reeval_ci_sz = inc_timeout_ci_size(coeffs, seed_res);
+        print_row(seed_res, current_seed, cont_ci_sz, reeval_ci_sz);
+
+        while (seed_res.intractables() == 0 || cont_ci_sz < reeval_ci_sz) {
+            solve_seed(current_seed, timeout, seed_res);
+
+            cont_ci_sz = continue_ci_size(coeffs, seed_res, timeout);
+            reeval_ci_sz = inc_timeout_ci_size(coeffs, seed_res);
+
+            print_row(seed_res, current_seed, cont_ci_sz, reeval_ci_sz);
+            current_seed++;
+        }
+
+        timeout *= 2;
+
+        seed_results intract_seed_res;
+        print_begin_re_eval_msg(timeout.count());
+        for (auto intract_it  = begin(seed_res.intractable)
+                ; intract_it != end  (seed_res.intractable)
+                ; intract_it++) {
+            solve_seed(*intract_it, timeout, intract_seed_res);
+            print_row(intract_seed_res, *intract_it, -1, -1);
+        }
+        print_end_re_eval_msg();
+
+        seed_res.add_intractable_results(intract_seed_res);
+        coeffs.update(intract_seed_res, timeout);
+    }
+}
+
+void solvability_calc::solve_seed(int seed, millisec timeout,
+                                          seed_results& seed_res) {
+    game_state gs(rules, seed);
     solver sol(gs);
     atomic<bool> terminate_solver(false);
 
@@ -66,46 +137,211 @@ void solvability_calc::solve_seed() {
     future_status status;
     do {
         status = future.wait_for(timeout);
+        auto start = chrono::steady_clock::now();
 
         if (status == future_status::timeout) {
             terminate_solver = true;
         } else if (status == future_status::ready) {
+
             if (terminate_solver) {
-                intractable++;
-            } else if (future.get()) {
-                solvable++;
+                seed_res.intractable.push_back(seed);
             } else {
-                unsolvable++;
+                auto end = chrono::steady_clock::now();
+                millisec elapsed_millis =
+                        chrono::duration_cast<chrono::milliseconds>(end - start);
+
+                if (future.get()) {
+                    seed_res.solvable.push_back(seed);
+                    seed_res.solvable_time += elapsed_millis;
+                } else {
+                    seed_res.unsolvable.push_back(seed);
+                    seed_res.unsolvable_time += elapsed_millis;
+                }
             }
         }
     } while (status != future_status::ready);
 }
 
-double solvability_calc::sol_lower_bound() {
-    int n = solvable + unsolvable + intractable;
-    int x = solvable;
-    double z = 1.96;
+double solvability_calc::continue_ci_size(inc_t_coeffs coeffs,
+                                          const seed_results &seed_res,
+                                          millisec timeout) const {
+    // If there are no intractable seeds yet, returns 0 (i.e. min interval),
+    // as continuing is only option
+    if (seed_res.intractables() == 0) return 0;
+
+    // Calculates the expected time to solve the intractable seeds with the
+    // timeout doubled
+    millisec inc_t_time(
+            rnd(seed_res.intractables() * coeffs.time * 2 * timeout.count())
+    );
+
+    // Calculates the expected number of seeds we would have attempted to solve
+    // were we to continue our search (i.e. without doubling timeout) in the
+    // above time period
+    millisec time_so_far = seed_res.solvable_time
+                           + seed_res.unsolvable_time
+                           + seed_res.intractables() * timeout;
+    int total_seeds = seed_res.solvables() + seed_res.unsolvables() + seed_res.intractables();
+    auto total_seeds_after = static_cast<int>(total_seeds * (1 + inc_t_time / time_so_far));
+
+    // Use what we know so far about the solvable/unsolvable instances to
+    // estimate the mean solvable/unsolvable
+    double mean_solvable    = agresti_coull_mean(seed_res.solvables()   , total_seeds);
+    double mean_unsolvable  = agresti_coull_mean(seed_res.unsolvables() , total_seeds);
+    double mean_intractable = agresti_coull_mean(seed_res.intractables(), total_seeds);
+
+    // Normalise
+    double mean_totals = mean_solvable + mean_unsolvable + mean_intractable;
+    mean_solvable /= mean_totals; mean_unsolvable /= mean_totals; mean_intractable /= mean_totals;
+
+    // Calculates the expected number of solvable/unsolvable/intractable if we continue
+    // for the length of time that would have been used to solve the intractable
+    // seeds with the timeout doubled
+    int total_solvable    = rnd(mean_solvable    * total_seeds_after);
+    int total_unsolvable  = rnd(mean_unsolvable  * total_seeds_after);
+    int total_intractable = rnd(mean_intractable * total_seeds_after);
+
+    // Given the above hypothetical values, calculates the size of the
+    // confidence interval we would be left with and returns it
+    return sol_ci_size(total_solvable, total_unsolvable, total_intractable);
+}
+
+double solvability_calc::inc_timeout_ci_size(inc_t_coeffs coeffs,
+                                             const seed_results &seed_res)
+const {
+    // Calculates the expected number of solvable/unsolvable/intractable
+    // if we run the solver on the intractable instances with double the timeout
+    int expected_solvable    = rnd(coeffs.solvable    * seed_res.intractables());
+    int expected_unsolvable  = rnd(coeffs.unsolvable  * seed_res.intractables());
+    int expected_intractable = rnd(coeffs.intractable * seed_res.intractables());
+
+    // Given the above values, calculates the total number of
+    // solvable/unsolvable/intractable we would expect to have afterwards
+    int total_solvable    = seed_res.solvables()   + expected_solvable   ;
+    int total_unsolvable  = seed_res.unsolvables() + expected_unsolvable ;
+    int total_intractable =                          expected_intractable;
+
+    // Given the above hypothetical values, calculates the size of the
+    // confidence interval we would be left with and returns it
+    return sol_ci_size(total_solvable, total_unsolvable, total_intractable);
+}
+
+
+////////////////////////
+// SEED RESULTS CLASS //
+////////////////////////
+
+solvability_calc::seed_results::seed_results()
+        : solvable_time(0), unsolvable_time(0) {
+}
+
+int solvability_calc::seed_results::solvables() const {
+    return static_cast<int>(solvable.size());
+}
+
+int solvability_calc::seed_results::unsolvables() const {
+    return static_cast<int>(unsolvable.size());
+}
+
+int solvability_calc::seed_results::intractables() const {
+    return static_cast<int>(intractable.size());
+}
+
+void solvability_calc::seed_results::add_intractable_results(
+        const seed_results &ir) {
+    solvable  .insert(end(solvable)  , begin(ir.solvable)  , end(ir.solvable  ));
+    unsolvable.insert(end(unsolvable), begin(ir.unsolvable), end(ir.unsolvable));
+    solvable_time   += ir.solvable_time  ;
+    unsolvable_time += ir.unsolvable_time;
+
+    intractable = ir.intractable;
+}
+
+solvability_calc::inc_t_coeffs::inc_t_coeffs(double solv, double intract,
+                                             double ti)
+        : solvable(solv), unsolvable(1-solv-intract), intractable(intract),
+          time(ti) {
+}
+
+// Updates the coefficients that represent the expected results of running
+// the solver on the intractable seeds with a doubled timeout. These updates
+// consist of calculating exponentially-wieghted moving averages.
+void solvability_calc::inc_t_coeffs::update(const seed_results &res,
+                                            solvability_calc::millisec timeout) {
+    int total_seeds = res.solvables() + res.unsolvables() + res.intractables();
+
+    double mean_solvable    = agresti_coull_mean(res.solvables()   , total_seeds);
+    double mean_unsolvable  = agresti_coull_mean(res.unsolvables() , total_seeds);
+    double mean_intractable = agresti_coull_mean(res.intractables(), total_seeds);
+
+    // Normalise
+    double mean_totals = mean_solvable + mean_unsolvable + mean_intractable;
+    mean_solvable /= mean_totals; mean_unsolvable /= mean_totals; mean_intractable /= mean_totals;
+
+    // The time taken if all intractable instances were still intractable with
+    // the doubled timeout
+    millisec all_timeout_time = timeout * total_seeds;
+    millisec total_time = res.solvable_time
+                        + res.unsolvable_time
+                        + res.intractables() * timeout;
+    double time_frac = total_time / all_timeout_time;
+
+    double w = coeff_ewma_weight, w_ = 1 - w;
+    solvable    = w * mean_solvable    + w_ * solvable   ;
+    unsolvable  = w * mean_unsolvable  + w_ * unsolvable ;
+    intractable = w * mean_intractable + w_ * intractable;
+    time        = w * time_frac        + w_ * time       ;
+}
+
+
+/////////////////////////
+// CALCULATION METHODS //
+/////////////////////////
+
+double solvability_calc::sol_lower_bound(int solvables,
+                                         int unsolvables,
+                                         int intractables) const {
+    int x = solvables;
+    int n = solvables + unsolvables + intractables;
     bool lower_bound = true;
-    return agrestiCoull(n, x, z, lower_bound);
+    return agresti_coull(x, n, lower_bound);
 }
 
-double solvability_calc::sol_upper_bound() {
-    int n = solvable + unsolvable + intractable;
-    int x = solvable + intractable;
-    double z = 1.96;
+double solvability_calc::sol_upper_bound(int solvables,
+                                         int unsolvables,
+                                         int intractables) const {
+    int x = solvables + intractables;
+    int n = solvables + unsolvables + intractables;
     bool lower_bound = false;
-    return agrestiCoull(n, x, z, lower_bound);
+    return agresti_coull(x, n, lower_bound);
 }
 
-double solvability_calc::agrestiCoull(int n, int x, double z, bool lower_bound) {
+double solvability_calc::sol_ci_size(int solvables,
+                                     int unsolvables,
+                                     int intractables) const {
+    return sol_upper_bound(solvables, unsolvables, intractables)
+           - sol_lower_bound(solvables, unsolvables, intractables);
+}
+
+double solvability_calc::agresti_coull(int x, int n, bool lower_bound) {
+    double z = 1.96;
     double n_ = n + pow(z, 2);
     double p = (x + pow(z, 2)/2)/n_;
-    double v = z * sqrt((p - pow(p, 2))/n_);
+    double half_ci = z * sqrt((p - pow(p, 2))/n_);
     if (lower_bound) {
-        double ans = p - v;
+        double ans = p - half_ci;
         return ans < 0 ? 0 : ans;
     } else {
-        double ans = p + v;
+        double ans = p + half_ci;
         return ans > 1 ? 1 : ans;
     }
 }
+
+double solvability_calc::agresti_coull_mean(int x, int n) {
+    double z = 1.96;
+    double n_ = n + pow(z, 2);
+    double p = (x + pow(z, 2)/2)/n_;
+    return p;
+}
+
+int solvability_calc::rnd(double d) { return static_cast<int>(lround(d)); }
--- src/main/solvability/solvability_calc.h
+++ src/main/solvability/solvability_calc.h
@@ -5,6 +5,8 @@
 #ifndef SOLVITAIRE_SOLVABILITY_H
 #define SOLVITAIRE_SOLVABILITY_H
 
+#include <vector>
+
 #include "../game/sol_rules.h"
 
 class solvability_calc {
@@ -14,21 +16,53 @@ public:
     void calculate_solvability_percentage();
 
 private:
-    void print_header() const;
-    void print_row(double, double) const;
-    void solve_seed();
-    void reset();
+    typedef std::chrono::milliseconds millisec;
 
-    double sol_lower_bound();
-    double sol_upper_bound();
-    static double agrestiCoull(int, int, double, bool);
+    struct seed_results {
+        seed_results();
+        int solvables() const;
+        int unsolvables() const;
+        int intractables() const;
+        void add_intractable_results(const seed_results&);
+
+        std::vector<int> solvable;
+        std::vector<int> unsolvable;
+        std::vector<int> intractable;
+        millisec solvable_time;
+        millisec unsolvable_time;
+    };
+
+    struct inc_t_coeffs {
+        inc_t_coeffs(double, double, double);
+        double solvable;    // Expected solvable    if we double the timeout
+        double unsolvable;  // Expected unsolvable  if we double the timeout
+        double intractable; // Expected intractable if we double the timeout
+        double time;        // Expected time taken  if we double the timeout (as
+                            // fraction of timeout)
+        void update(const seed_results&, millisec);
+    };
+
+    // Printing methods
+    void print_header(long) const;
+    void print_row(const seed_results&, int, double, double) const;
+    void print_begin_re_eval_msg(long) const;
+    void print_end_re_eval_msg() const;
+
+    // Solving methods
+    void solve_seed(int, millisec, seed_results&);
+    double continue_ci_size(inc_t_coeffs, const seed_results &, millisec) const;
+    double inc_timeout_ci_size(inc_t_coeffs, const seed_results &) const;
+
+    // Calculation methods
+    double sol_lower_bound(int, int, int) const;
+    double sol_upper_bound(int, int, int) const;
+    double sol_ci_size(int, int, int) const;
+    static double agresti_coull(int, int, bool);
+    static double agresti_coull_mean(int, int);
+    static int rnd(double);
 
     const sol_rules& rules;
-    int current_seed;
-    std::chrono::milliseconds timeout;
-    int solvable;
-    int unsolvable;
-    int intractable;
+    const static double coeff_ewma_weight;
 };
 
 
