d7ae7d76ec4f2cc641c8b1db05490d2080525bb7
Charles Blake <cb307@theory.cs.st-andrews.ac.uk>
2018-06-22 10:23:27 +0100

    Modify gen results script and remove results dir

--- generate-results.sh
+++ generate-results.sh
@@ -1,9 +1,62 @@
 #!/bin/bash
 
 declare -a arr=(
-"somerset"
 "alpha-star"
+"bakers-game"
+"black-hole"
+"castles-of-spain"
+"delta-star"
+"eight-off-any-card-spaces"
+"eight-off"
+"fan"
+"flower-garden"
+"fore-cell"
+"fortunes-favor"
+"king-albert"
+"northwest-territory"
+"one-cell"
+"raglan"
+"sea-towers"
+"somerset"
+"spanish-patience"
+"spiderette"
+"two-cell"
+"will-o-the-wisp"
+"free-cell-0-cell"
+"free-cell-1-cell"
+"free-cell-2-cell"
+"free-cell-3-cell"
+"free-cell-4-pile"
+"free-cell-5-pile"
+"free-cell-6-pile"
+"free-cell-7-pile"
+"free-cell"
 )
+
+### Presets not attempted:
+
+## Rules incorrect
+
+#"martha"
+#"canfield"
+#"penguin"
+
+## Too difficult (?)
+
+#"alina"
+#"bakers-dozen"
+#"scotch-patience"
+#"spider"
+
+## Stock games
+
+#"blind-alleys"
+#"chameleon"
+#"duchess"
+#"east-haven"
+#"klondike"
+#"klondike-deal-1"
+
 
 DATE=`date +%Y-%m-%d-%H-%M-%S`
 
@@ -12,7 +65,7 @@ mkdir -p results/$DATE
 for i in "${arr[@]}"
 do
     echo "Running $i ..."
-    ./sol --type "$i" --solvability 10000 --time 3600000 --cores 50 --stream > results/$DATE/$i.csv
+    ./sol --type "$i" --solvability 10000 --time 600000 --cores 25 --stream > results/$DATE/$i.csv
 done
 
 echo "Done!"
