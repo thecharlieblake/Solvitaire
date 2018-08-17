691a5e7421a824da76203f3a680a92bfb251fd13
Charles Blake <cb307@babbage.cs.st-andrews.ac.uk>
2018-08-17 17:28:55 +0100

    Add linux time command results to output

--- solvability.sh
+++ solvability.sh
@@ -12,10 +12,11 @@ cores="$2"
 sol_command="$3"
 minramfree="100G"
 out="$4"
+time_command="/usr/bin/time -f \" %e, %U, %S\""
+combine_command="sed -e 'H;\${x;s/\n/,/g;s/^,//;p;};d'"
 
+echo "Attempted Seed, Outcome, Time Taken(ms), States Searched, Unique States Searched, Backtracks, Dominance Moves, States Removed From Cache, Final States In Cache, Final Buckets In Cache, Maximum Search Depth, Final Search Depth, real (time), user (time), sys (time) " > "$out.csv"
 
-echo "Attempted Seed, Outcome, Time Taken(ms), States Searched, Unique States Searched, Backtracks, Dominance Moves, States Removed From Cache, Final States In Cache, Final Buckets In Cache, Maximum Search Depth, Final Search Depth" > "$out.csv"
-
-seq "$seeds" | parallel --joblog $out.log --progress --memfree $minramfree -j"$cores" "$sol_command" --ra {} --class  >> "$out.csv" 
+seq "$seeds" | parallel --joblog $out.log --progress --memfree $minramfree -j"$cores" "exec $time_command $sol_command --ra {} --class 2>&1 | $combine_command >> $out.csv"
 
 
