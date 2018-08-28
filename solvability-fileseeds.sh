17fc55202330a4d5456c3518e25987e776efd522
Ian Gent <ipg@babbage.cs.st-andrews.ac.uk>
2018-08-28 14:51:31 +0100

    new klondike variant

--- solvability-fileseeds.sh
+++ solvability-fileseeds.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
-
 if [ "$#" != 4 ]; then
-    echo "Usage: <filename> <cores> \"<solvitaire_command>\" <output_file>"
-    echo "e.g.: ./solvability.sh reruns.txt 40 \"sol --type alpha-star --timeout 6000 --str smart-solvability\" results"
+    echo "Usage: <seeds-filename> <cores> \"<solvitaire_command>\" <output_file>"
+    echo "e.g.: ./solvability-fileseeds.sh listOfSeeds.txt 40 \"./sol --type alpha-star --timeout 6000 --str smart-solvability\" results"
     exit 1
 fi
 
 seeds="$1"
 cores="$2"
 sol_command="$3"
-minramfree="100G"
 out="$4"
 
+time_command="/usr/bin/time -f \" %e, %U, %S, %M\""
+combine_command="sed -e 'H;\${x;s/\n/,/g;s/^,//;p;};d'"
 
-echo "Attempted Seed, Outcome, Time Taken(ms), States Searched, Unique States Searched, Backtracks, Dominance Moves, States Removed From Cache, Final States In Cache, Final Buckets In Cache, Maximum Search Depth, Final Search Depth" > "$out.csv"
+minramfree="100G"
+completejobs=4
 
-cat $seeds | parallel --joblog $out.log --progress --memfree $minramfree -j"$cores" "$sol_command" --ra {} --class  >> "$out.csv" 
+echo "CL: $0 $@" >> "$out.experiment"
+echo "Host: $HOSTNAME: " >> "$out.experiment"
+echo "StartDate: " `date` >> "$out.experiment"
+
+echo "Attempted Seed, Outcome, Time Taken(ms), States Searched, Unique States Searched, Backtracks, Dominance Moves, States Removed From Cache, Final States In Cache, Final Buckets In Cache, Maximum Search Depth, Final Search Depth, real (time), user (time), sys (time) " >> "$out.experiment"
+
+cat $seeds | parallel --nice 4 --joblog $out.log --progress --memfree $minramfree -j"$cores" "exec $time_command $sol_command --ra {} --class 2>&1 | $combine_command >> $out.csv"
+echo "MainRunEndDate: " `date` >> "$out.experiment"
+
+parallel --nice 4 --joblog $out.log --progress --retry-failed -j"$completejobs" 2>&1 
+
+echo "EndDate: " `date` >> "$out.experiment"
 
 
