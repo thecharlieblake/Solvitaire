6ad0fd142fc42a6fdac838efd2069cb92b6f3b64
Ian Gent <ipg@theory.cs.st-andrews.ac.uk>
2018-10-24 17:04:14 +0100

    use revealmove as optimisation for allowing reverse

--- src/main/game/search-state/game_state.legal_moves.cpp
+++ src/main/game/search-state/game_state.legal_moves.cpp
@@ -86,7 +86,7 @@ vector<move> game_state::get_legal_moves(move parent_move) {
     if (rules.foundations_removable) {
         for (auto f : foundations) {
 	// have to allow immediate reversal of worry back if it might have turned up a card
-            if (piles[f].empty() || ((parent_move.to == f) && (rules.face_up == fu::TOP_CARDS)) || dominance_blocks_foundation_move(f)) continue;
+            if (piles[f].empty() || ((parent_move.to == f) && !parent_move.reveal_move) || dominance_blocks_foundation_move(f)) continue;
 
             add_valid_tableau_moves(moves, f);
 
@@ -129,9 +129,9 @@ vector<move> game_state::get_legal_moves(move parent_move) {
 
 //     if (rules.move_built_group != bgt::WHOLE_PILE) {
         for (auto t_from : tableau_piles) {
-            // Forbids moves from empty piles, reversing parent moves (unless it might have turned a card), or from single-
+            // Forbids moves from empty piles, reversing parent moves (unless it turned a card), or from single-
             // card piles to empty piles
-            if (piles[t_from].empty() || ((parent_move.to == t_from) && (rules.face_up != fu::TOP_CARDS))|| tableau_space_and_auto_reserve()) continue;
+            if (piles[t_from].empty() || ((parent_move.to == t_from) && !parent_move.reveal_move)|| tableau_space_and_auto_reserve()) continue;
 
             for (auto to : tableau_piles) {
                 if (is_valid_tableau_move(t_from, to)
